eb78326769a1328b60a1d46b6dfe0d52d8cb87d7
kfy6pyf79t-max <kfy6pyf79t@privaterelay.appleid.com>
2026-08-04 10:43:12 +0700

    Добавил id для якорной навигации в необходимых блоках

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
     const currentYear = new Date().getFullYear();
-  
+    
     return (
       <footer className="bg-slate-950 text-slate-400 pt-16 pb-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
